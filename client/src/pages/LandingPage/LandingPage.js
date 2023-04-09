390a2d7b37c5ed172f436f2826449e64ace8d03b
Jessica Arrais <jessicalarrais@gmail.com>
2023-04-09 11:38:08 -0400

    add landing-page structure and styling

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,13 +1,58 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
+import aptIcon from "../../assets/icons/apt-icon.png";
+import houseIcon from "../../assets/icons/house-icon.png";
+import petIcon from "../../assets/icons/pet-icon.png";
+import "./LandingPage.scss";
 
 export default function LandingPage() {
   const navigate = useNavigate();
 
   return (
-    <Button
-      text="Check Our Properties"
-      handleOnClick={() => navigate("/listings")}
-    />
+    <main className="landing-page">
+      <div className="landing-page__body">
+        <h1 className="landing-page__page-header">Find your new place!</h1>
+        <input
+          className="landing-page__searchbar"
+          type="text"
+          // onChange={handleOnSearch}
+          // value={searchKeyword}
+          placeholder="Where?"
+        />
+        <div className="landing-page__filters">
+          <input className="landing-page__calendar" type="date" />
+          <Button
+            icon={houseIcon}
+            emphasis="low-emphasis"
+            // handleOnClick={(e) => handleOnFilter(e, "type", "house")}
+          />
+          <Button
+            icon={aptIcon}
+            emphasis="low-emphasis"
+            // handleOnClick={(e) => handleOnFilter(e, "type", "apartment")}
+          />
+          <Button
+            icon={petIcon}
+            emphasis="low-emphasis"
+            // handleOnClick={(e) => handleOnFilter(e, "pets", 1)}
+          />
+          <Button
+            emphasis="low-emphasis"
+            text="Price $"
+            // handleOnClick={(e) => handleOnSort(e, "lower")}
+          />
+          <Button
+            emphasis="low-emphasis"
+            text="Price $$$$"
+            // handleOnClick={(e) => handleOnSort(e, "higher")}
+          />
+        </div>
+        <Button
+          text="Check Our Properties"
+          emphasis="high-emphasis"
+          handleOnClick={() => navigate("/listings")}
+        />
+      </div>
+    </main>
   );
 }
